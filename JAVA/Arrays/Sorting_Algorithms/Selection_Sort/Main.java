45dcf31c882859282a066ab63e5f517d00691483
conx42 <97751032+conx42@users.noreply.github.com>
2024-03-17 00:50:39 +0530

    Selection Sort - SubOptimized with proper intution(explanation)

--- JAVA/Arrays/Sorting_Algorithms/Selection_Sort/Main.java
+++ JAVA/Arrays/Sorting_Algorithms/Selection_Sort/Main.java
@@ -37,20 +37,32 @@ class Swap{
 	}
 }
 class Selection_Sort{
-	
+	/*
+		Intution of this Selection Sort(Ascending Order) Algorithm is ->
+		step-1: take maximum element	
+		step-2: place the maximum element at last
+		step-3: shrink the unsorted array
+		Again,
+			step-4: take maximum element from the unsortd array
+			step-5:	place the maximum element at the last of the unsorted array
+			step-6:	shrink the unsorted array
+			---
+		step-n: Do, this process until the unsorted array eradicate completely
+							~~~ by this way the entire array will be sorted using Selection sort
+	*/
 	public void Sort(int []nums, int n){
 		int max[]=new int[2];
-		for(int i=(n-1); i>=0; --i){
+		for(int i=(n-1); i>=1; --i){
 			
 			int start_index=0; // start always with 0th index - because sorting the array from last element
 			
 			int end_index=i;
 			
 			// find maximum element from the unsorted array (⬇⬇⬇⬇)
-			max = getMax.Find_Maximum_Element(nums, start_index,end_index);
+			max = getMax.Find_Maximum_Element(nums, start_index,end_index); //step-1: take the maximum element from the unsorted array.
 			
 			// max[0] = maximun element index of the remaining array
-			Swap s=new Swap(nums,max[0],end_index);
+			Swap s=new Swap(nums,max[0],end_index); //step-2: place the maximum element at last_index of the unsorted array.
 		}
 	}
 }
