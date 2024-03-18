3e8f6004d8b711e9d249b91799cbb2ad238be0e4
conx42 <97751032+conx42@users.noreply.github.com>
2024-03-18 13:46:51 +0530

    Insertion_Sort -> My-New-Design including 'i--' && 'break;' in algorithm

---
+++ JAVA/Arrays/Sorting_Algorithms/Insertion_Sort/Insertion_Sort_New_Design_1/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Array_Input{
+    static int [] numsIO(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size of the array: ");
+        int n=sc.nextInt();
+        int []nums=new int[n];
+        for (int i=0; i<n; i++) {
+            System.out.print("Enter Array("+i+"): ");
+            nums[i]=sc.nextInt();
+        }
+        return nums;
+    }
+}
+class Swap{
+    static void swap(int []nums, int x, int y){
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+}
+class Insertion_Sort{
+    static int []Sort(int []nums, int n){
+        
+        for (int i=0; i<=n-2; i++) {
+            for (int j=i+1; j>=1; j--) { //This inner-Loop will terminate when 'j=0' then only.
+                if(nums[j]<nums[i]){
+                    Swap.swap(nums, j, i);
+                }else{ //no need to iterate again in the sorted array.
+                    break; //important while using 'i--' in algorithm.
+                }
+                i--;
+            }
+        }
+
+        return nums;
+    }
+}
+public class Main{
+    public static void main(String []args){
+        int []nums = Array_Input.numsIO();        
+        // System.out.println(Arrays.toString(nums));
+        System.out.println("Before sorting the array is: "+Arrays.toString(nums));
+        int []sorted_nums=Insertion_Sort.Sort(nums, nums.length);
+        System.out.println("After sorting the array is: "+Arrays.toString(sorted_nums));
+    }
+}
